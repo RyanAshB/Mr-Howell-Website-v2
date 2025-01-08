8cebbe9bb91eae43df9b7a36ac0275ba11a7fd89
Ryan Badaloo <ryanash.b@gmail.com>
2025-01-08 11:44:36 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/ticket')
+def ticket():
+    return render_template('ticket.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
